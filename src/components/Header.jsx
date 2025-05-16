555e8f2f08fa85847466fda11b7eb1e5fa87ad23
Asasui2440 <nekocat3838@gmail.com>
2025-05-17 00:03:13 +0900

    画像ファイル間違い修正

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,7 +2,7 @@ export default function Header() {
   return (
     <div className="absolute top-0 left-0 w-full h-full -z-10 overflow-hidden">
       <img
-        src="/Image_fx (6).jpg"
+        src="/Image_fx(6).jpg"
         alt="TEAra header"
         className="w-full h-full object-cover"
         style={{ objectPosition: "center" }} // 中央部分を表示
